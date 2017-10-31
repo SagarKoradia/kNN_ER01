f687fc4a0a674a21bf0ed9bb47f569b8d2b7d1ed
Sagar Koradia <31327738+SagarKoradia@users.noreply.github.com>
2017-11-01 03:54:33 +1100

    Update code.py

--- code.py
+++ code.py
@@ -9,9 +9,9 @@ X = dataset[:, 0:23].astype(float)
 Y = dataset[:, 23]
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=25)
 
-knn = KNeighborsClassifier(n_neighbors=3)
+knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, y_train)
-KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski', metric_params=None, n_jobs=1, n_neighbors=6, p=2, weights='uniform')
+KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski', metric_params=None, n_jobs=1, p=2, weights='uniform')
 prediction = knn.predict(X_test)
 
 print('Prediction: \n {}'.format(prediction))
